de36a7fb304a661bd7aae40206ed9795e1a2d3a5
Andrew Smith <meuzishun@gmail.com>
2022-07-30 17:54:01 -0400

    Create a way to get posts from users and posts

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Post() {
-  return <div>Post</div>;
+function Post({ post }) {
+  return (
+    <div>
+      <p>Title: {post.title}</p>
+      <p>Content: {post.content}</p>
+    </div>
+  );
 }
 
 export default Post;
--- src/pages/home/timeline/Timeline.js
+++ src/pages/home/timeline/Timeline.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Post from '../../../components/Post';
+import fake_posts from '../../../fake_data/fake_posts';
 
 function Timeline({ user }) {
+  const [userPosts, setUserPosts] = useState([]);
+
+  useEffect(() => {
+    const posts = fake_posts.filter((post) => post.author === user.id);
+    console.log(posts);
+    setUserPosts(posts);
+  }, []);
   return (
     <div>
-      {user.posts.map((post) => (
-        <p key={post}>post id: {post}</p>
-      ))}
+      {userPosts
+        ? userPosts.map((post) => {
+            return <Post key={post} post={post} />;
+          })
+        : null}
     </div>
   );
 }
