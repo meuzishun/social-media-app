ca7794d87aa3ed211019851b2bfde6d3e6d8c872
Andrew Smith <meuzishun@gmail.com>
2022-07-31 10:37:33 -0400

    General cleanup

--- src/pages/home/feed/Feed.js
+++ src/pages/home/feed/Feed.js
@@ -1,24 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Post from '../../../components/Post';
-import fake_posts from '../../../fake_data/fake_posts';
-import fake_users from '../../../fake_data/fake_users';
 
 function Feed({ friendPostIds }) {
-  // const [friendPosts, setFriendPosts] = useState([]);
-
-  // useEffect(() => {
-  // const friends = friendIds.map((friendId) =>
-  //   fake_users.find((user) => user.id === friendId)
-  // );
-  // const friendPosts = friends.map((friend) => friend.posts);
-  // setFriendPosts(friendPosts.flat());
-  // setFriendPosts(
-  // friendPostIds.map((postId) =>
-  // fake_posts.find((post) => post.id === postId)
-  // )
-  // );
-  // }, []);
-
   return (
     <div>
       {friendPostIds
