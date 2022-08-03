af6d461e8fad15ffd03909929180712632bc5192
Andrew Smith <meuzishun@gmail.com>
2022-08-03 13:34:44 -0400

    Make sure to clear reply when submitting

--- src/components/Post.js
+++ src/components/Post.js
@@ -24,6 +24,7 @@ function Post({ post }) {
   const handleReplySubmit = (e) => {
     e.preventDefault();
     submitPostReply(post, replyContent);
+    setReplyContent('');
     setReplyState(false);
   };
 
