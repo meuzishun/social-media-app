9cfdc3b35360d4a070ba6400aefa5a6e706ab11f
Andrew Smith <meuzishun@gmail.com>
2022-08-17 10:57:57 -0400

    Make a real footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>Copyright &copy; meuzishun 2022</p>
+    </footer>
+  );
 }
 
 export default Footer;
