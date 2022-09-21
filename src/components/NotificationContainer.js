cc995b33da5c1194257dca35bfc6905ef67577e2
Andrew Smith <meuzishun@gmail.com>
2022-09-21 19:39:17 -0400

    No longer using useEffect in NotificationContainer.js

--- src/components/NotificationContainer.js
+++ src/components/NotificationContainer.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import styles from './NotificationContainer.module.css';
 import Notification from './Notification';
 import { NotificationsContext } from '../App';
