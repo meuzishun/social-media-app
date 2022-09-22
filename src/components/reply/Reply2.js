6bc8843b5b88b2e18678fac068fe7ac4a9de96d0
Andrew Smith <meuzishun@gmail.com>
2022-09-21 20:06:55 -0400

    Remove commented out code from Reply2.js

--- src/components/reply/Reply2.js
+++ src/components/reply/Reply2.js
@@ -19,20 +19,14 @@ function Reply2({ reply }) {
   };
 
   const handleShowRepliesClick = () => {
-    // repliesContainer.current.classList.remove('hidden');
     repliesContainer.current.style = 'display: block;';
-    // hideRepliesBtn.current.classList.remove('hidden');
     hideRepliesBtn.current.style = 'display: block;';
-    // showRepliesBtn.current.classList.add('hidden');
     showRepliesBtn.current.style = 'display: none;';
   };
 
   const handleHideRepliesClick = () => {
-    // repliesContainer.current.classList.add('hidden');
     repliesContainer.current.style = 'display: none;';
-    // hideRepliesBtn.current.classList.add('hidden');
     hideRepliesBtn.current.style = 'display: none;';
-    // showRepliesBtn.current.classList.remove('hidden');
     showRepliesBtn.current.style = 'display: block;';
   };
 
