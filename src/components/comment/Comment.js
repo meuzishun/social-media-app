2b4335ee276783873abc79f2c3d6551cfc83965e
Andrew Smith <meuzishun@gmail.com>
2022-09-21 20:04:35 -0400

    Remove commented out code from Comment.js

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -20,20 +20,14 @@ function Comment({ comment }) {
   };
 
   const handleShowRepliesClick = () => {
-    // repliesContainer.current.classList.remove('hidden');
     repliesContainer.current.style = 'display: block;';
-    // hideRepliesBtn.current.classList.remove('hidden');
     hideRepliesBtn.current.style = 'display: block;';
-    // showRepliesBtn.current.classList.add('hidden');
     showRepliesBtn.current.style = 'display: none;';
   };
 
   const handleHideRepliesClick = () => {
-    // repliesContainer.current.classList.add('hidden');
     repliesContainer.current.style = 'display: none;';
-    // hideRepliesBtn.current.classList.add('hidden');
     hideRepliesBtn.current.style = 'display: none;';
-    // showRepliesBtn.current.classList.remove('hidden');
     showRepliesBtn.current.style = 'display: block;';
   };
 
